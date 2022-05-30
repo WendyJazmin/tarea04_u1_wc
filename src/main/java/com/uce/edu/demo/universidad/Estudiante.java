0af39d5544a07cfad7fe55eb9883a502f8c132c9
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-05-30 05:48:51 -0500

    Tarea 4 subida desde consola

--- src/main/java/com/uce/edu/demo/universidad/Estudiante.java
+++ src/main/java/com/uce/edu/demo/universidad/Estudiante.java
@@ -1,5 +1,32 @@
 package com.uce.edu.demo.universidad;
 
+import java.time.LocalDateTime;
+
 public class Estudiante {
 
+	private String name;
+	private String apellido;
+	private LocalDateTime fechaNacimiento;
+	
+	//get y set
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	
 }
