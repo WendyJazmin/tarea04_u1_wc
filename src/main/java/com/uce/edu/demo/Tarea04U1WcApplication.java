f0be00026fa7bffd955585950b41c868ae4aad4c
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-05-30 05:09:40 -0500

    tarea 4 subida desde consola

---
+++ src/main/java/com/uce/edu/demo/Tarea04U1WcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea04U1WcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea04U1WcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/demo/Tarea04U1WcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea04U1WcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
